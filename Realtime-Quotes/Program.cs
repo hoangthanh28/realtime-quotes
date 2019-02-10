25d567b641e4e09a06d6b3e513d97b11cecd59b9
Thanh Tran <thanh.tran@vn.devinition.com>
2019-02-10 11:57:01 +0700

    - Remove un-use dependency and library.

--- Realtime-Quotes/Program.cs
+++ Realtime-Quotes/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,11 +15,14 @@ namespace Realtime_Quotes
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseKestrel()
+                .Build();
+
     }
 }
